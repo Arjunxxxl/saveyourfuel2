c94a28bb2f20a0647df7fe50b7fd1ab175b0dd28
arjunxxxl <a.asankhala@gmail.com>
2018-02-09 15:56:02 +0530

    second commit

--- app/src/main/java/com/saveyourfuel/saveyourfuel/loginActivity.java
+++ app/src/main/java/com/saveyourfuel/saveyourfuel/loginActivity.java
@@ -1,13 +1,40 @@
 package com.saveyourfuel.saveyourfuel;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
-public class loginActivity extends AppCompatActivity {
+public class loginActivity extends AppCompatActivity implements  View.OnClickListener{
+
+    CheckBox cemail,cpass;
+    TextView reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        cemail = findViewById(R.id.checkBox);
+        cpass = findViewById(R.id.checkBox2);
+        reg = findViewById(R.id.textView4);
+
+
+        cemail.setVisibility(View.INVISIBLE);
+        cpass.setVisibility(View.INVISIBLE);
+
+        reg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.textView4:
+                Intent i = new Intent(loginActivity.this, selectTyleActivity.class);
+                startActivity(i);
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/saveyourfuel/saveyourfuel/selectTyleActivity.java
@@ -0,0 +1,39 @@
+package com.saveyourfuel.saveyourfuel;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class selectTyleActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button consumer,trasnsport;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_tyle);
+        consumer = findViewById(R.id.button);
+        trasnsport = findViewById(R.id.button1);
+        consumer.setOnClickListener(this);
+        trasnsport.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.button :
+                Intent mainIntent = new Intent(selectTyleActivity.this, consumerREGActivity.class);
+                startActivity(mainIntent);
+                //selectTyleActivity.this.finish();
+                break;
+            case R.id.button1 :
+                Intent mainIntent1 = new Intent(selectTyleActivity.this, loginActivity.class);
+                startActivity(mainIntent1);
+                //selectTyleActivity.this.finish();
+                break;
+        }
+    }
+}
