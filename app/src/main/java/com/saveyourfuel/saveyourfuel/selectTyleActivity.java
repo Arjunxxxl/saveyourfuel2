4c87b5357835d0f36c5e144b2bd589ec61a5baeb
arjunxxxl <a.asankhala@gmail.com>
2018-02-28 22:15:18 +0530

    new mat design + small improvement

--- app/src/main/java/com/saveyourfuel/saveyourfuel/selectTyleActivity.java
+++ app/src/main/java/com/saveyourfuel/saveyourfuel/selectTyleActivity.java
@@ -5,25 +5,38 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-public class selectTyleActivity extends AppCompatActivity implements View.OnClickListener{
+public class selectTyleActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button consumer,trasnsport;
+    Button consumer, trasnsport;
     Toolbar toolbar;
 
     public static String FACEBOOK_URL = "https://www.facebook.com/iitp.ac.in/";
     public static String FACEBOOK_PAGE_ID = "iitp.ac.in";
-    ImageButton facebookbutton,website;
+    ImageButton facebookbutton, website;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+// clear FLAG_TRANSLUCENT_STATUS flag:
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+// finally change the color
+            window.setStatusBarColor(Color.parseColor("#000000"));
+        }
+
         setContentView(R.layout.activity_select_tyle);
         toolbar = findViewById(R.id.toolbar1);
         toolbar.setTitle(" ");
@@ -47,7 +60,7 @@ public class selectTyleActivity extends AppCompatActivity implements View.OnClic
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(selectTyleActivity.this,loginActivity.class);
+                Intent i = new Intent(selectTyleActivity.this, loginActivity.class);
                 startActivity(i);
                 selectTyleActivity.this.finish();
             }
@@ -56,14 +69,13 @@ public class selectTyleActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        switch (view.getId())
-        {
-            case R.id.button :
+        switch (view.getId()) {
+            case R.id.button:
                 Intent mainIntent = new Intent(selectTyleActivity.this, maincomsumerREGActivity.class);
                 startActivity(mainIntent);
                 selectTyleActivity.this.finish();
                 break;
-            case R.id.button1 :
+            case R.id.button1:
                 Intent mainIntent1 = new Intent(selectTyleActivity.this, consumerREGActivity.class);
                 startActivity(mainIntent1);
                 selectTyleActivity.this.finish();
